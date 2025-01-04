3e888bb37443b0e9d711daf945b12a86f82d6806
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-04 16:59:37 -0300

    Capitulo 1 Parte 3

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {files: ["**/*.{js,mjs,cjs,ts}"]},
+    {languageOptions: { globals: globals.node }},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules:{
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+            indent: ["error", 4],
+            "linebreak-style": ["error", "unix"],
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-empty-object-type": "off",
+        },
+        ignores: ["!dist/**/*", "!node_modules/"]
+    }
+];
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from "@nestjs/common";
+import { AppService } from "./app.service";
 
-@Controller('api/hello')
+@Controller("api/hello")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get('')
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Get("")
+    getHello(): string {
+        return this.appService.getHello();
+    }
 
   @Post()
   postHello(): string {
-    return this.appService.getHello();
+      return this.appService.getHello();
   }
 }
