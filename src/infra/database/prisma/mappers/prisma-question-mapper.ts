400a71673c8918f2c0ce3016806f1836c97c2877
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-05 20:01:40 -0300

    Capitulo 2 Parte 6

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -2,7 +2,7 @@ import { UniqueEntityId } from "@/core/entities/value-objects/unique-entity-id";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { QuestionAttachmentList } from "@/domain/forum/enterprise/entities/question-attachment-list";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
-import { Question as PrismaQuestion } from "@prisma/client";
+import { Prisma, Question as PrismaQuestion } from "@prisma/client";
 
 export class PrismaQuestionMapper {
     static toDomain(raw: PrismaQuestion) {
@@ -21,5 +21,20 @@ export class PrismaQuestionMapper {
             },
             new UniqueEntityId(raw.id),
         );
+    }
+
+    static toPersistence(
+        question: Question,
+    ): Prisma.QuestionUncheckedCreateInput {
+        return {
+            id: question.id.toString(),
+            title: question.title,
+            content: question.content,
+            authorId: question.authorId.toValue(),
+            bestAnswerId: question.bestAnswerId?.toValue(),
+            slug: question.slug.value,
+            createdAt: question.createdAt,
+            updatedAt: question.updatedAt,
+        };
     }
 }
--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -10,11 +10,18 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     constructor(private readonly prismaService: PrismaService) {}
 
     async create(question: Question) {
-        return this.prismaService.question.create;
+        const data = PrismaQuestionMapper.toPersistence(question);
+        this.prismaService.question.create({ data });
     }
 
-    findBySlug(slug: string): Promise<Question | null> {
-        throw new Error("Method not implemented.");
+    async findBySlug(slug: string) {
+        const question = await this.prismaService.question.findUnique({
+            where: { slug },
+        });
+        if (!question) {
+            return null;
+        }
+        return PrismaQuestionMapper.toDomain(question);
     }
 
     async findById(id: string) {
@@ -29,15 +36,31 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
         return PrismaQuestionMapper.toDomain(question);
     }
 
-    delete(question: Question): Promise<void> {
-        throw new Error("Method not implemented.");
+    async delete(question: Question) {
+        const data = PrismaQuestionMapper.toPersistence(question);
+        await this.prismaService.question.delete({
+            where: { id: data.id },
+        });
     }
 
-    save(question: Question): Promise<void> {
-        throw new Error("Method not implemented.");
+    async save(question: Question) {
+        const data = PrismaQuestionMapper.toPersistence(question);
+        await this.prismaService.question.update({
+            where: { id: question.id.toString() },
+            data,
+        });
     }
 
-    findManyRecent(params: PaginationParams): Promise<Question[]> {
-        throw new Error("Method not implemented.");
+    async findManyRecent({ page }: PaginationParams) {
+        const PAGE_SIZE = 20;
+        const questions = await this.prismaService.question.findMany({
+            orderBy: {
+                createdAt: "desc",
+            },
+            take: PAGE_SIZE,
+            skip: (page - 1) * PAGE_SIZE,
+        });
+
+        return questions.map(PrismaQuestionMapper.toDomain);
     }
 }
