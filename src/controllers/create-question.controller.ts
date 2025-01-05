7b49b9d9baec691ff95e3280e22971ef7ed20f7d
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-04 23:34:11 -0300

    Capitulo 1 Parte 16

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Post, UseGuards, UsePipes } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { CurrentUser } from "src/auth/current.user.decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UserPayload } from "src/auth/jwt.strategy";
 import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
 import { PrismaService } from "src/prisma/prisma.service";
 import { z } from "zod";
@@ -8,7 +10,7 @@ import { z } from "zod";
 
 
 const createQuestionBodySchema = z.object({
-    
+
 });
 
 type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
@@ -19,15 +21,15 @@ export class CreateQuestionController {
 
     constructor(
         private readonly prismaService: PrismaService,
-         private readonly jwt: JwtService
-    ) { }  
+        private readonly jwt: JwtService
+    ) { }
 
 
     @Post()
-    @UsePipes(new ZodValidationPipe(createQuestionBodySchema))  // apply zod validation pipe here.
-    async handle(@Body() body: CreateQuestionBodySchema) {
-        const { } = body;
-
+    @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
+    async handle(@Body() body: CreateQuestionBodySchema, @CurrentUser() user: UserPayload) {
+        // const { } = body;
+        console.log(user.sub);
         return "ok";
 
 
