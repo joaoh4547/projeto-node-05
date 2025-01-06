8c6894ae985d310cbea09a0486e619a373cdaaa9
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-06 12:31:27 -0300

    Capitulo 2 Parte 18

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { Env } from "@/infra/env";
 import { JwtStrategy } from "./jwt.strategy";
+import { APP_GUARD } from "@nestjs/core";
+import { JwtAuthGuard } from "./jwt-auth.guard";
 
 @Module({
     imports: [
@@ -26,7 +28,13 @@ import { JwtStrategy } from "./jwt.strategy";
             },
         }),
     ],
-    providers: [JwtStrategy],
+    providers: [
+        JwtStrategy,
+        {
+            provide: APP_GUARD,
+            useClass: JwtAuthGuard,
+        },
+    ],
     exports: [],
 })
 export class AuthModule {}
