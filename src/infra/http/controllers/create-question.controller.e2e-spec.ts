315e08be922c03110308e87fed1211cd5eb07a0f
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-11 11:50:16 -0300

    Capitulo 4 Parte 8

--- src/infra/http/controllers/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-question.controller.e2e-spec.ts
@@ -5,23 +5,26 @@ import { INestApplication } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
 import request from "supertest";
+import { AttachmentFactory } from "test/factories/make-attachment";
 import { StudentFactory } from "test/factories/make-student";
 
 describe("Create Question (E2E)", () => {
     let app: INestApplication;
     let studentFactory: StudentFactory;
+    let attachmentFactory: AttachmentFactory;
     let prismaService: PrismaService;
     let jwtService: JwtService;
 
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule, DatabaseModule],
-            providers: [StudentFactory],
+            providers: [StudentFactory, AttachmentFactory],
         }).compile();
 
         app = moduleRef.createNestApplication();
         prismaService = moduleRef.get(PrismaService);
         studentFactory = moduleRef.get(StudentFactory);
+        attachmentFactory = moduleRef.get(AttachmentFactory);
         jwtService = moduleRef.get(JwtService);
         await app.init();
     });
@@ -31,12 +34,19 @@ describe("Create Question (E2E)", () => {
 
         const accessToken = jwtService.sign({ sub: user.id.toString() });
 
+        const attachment1 = await attachmentFactory.makePrismaAttachment();
+        const attachment2 = await attachmentFactory.makePrismaAttachment();
+
         const response = await request(app.getHttpServer())
             .post("/questions")
             .set("Authorization", `Bearer ${accessToken}`)
             .send({
                 title: "Question Title",
                 content: "Question Content",
+                attachments: [
+                    attachment1.id.toString(),
+                    attachment2.id.toString(),
+                ],
             });
 
         expect(response.statusCode).toBe(201);
@@ -48,5 +58,13 @@ describe("Create Question (E2E)", () => {
         });
 
         expect(questionOnDatabase).toBeTruthy();
+
+        const attachmentsOnDatabase = await prismaService.attachment.findMany({
+            where: {
+                questionId: questionOnDatabase?.id,
+            },
+        });
+
+        expect(attachmentsOnDatabase).toHaveLength(2);
     });
 });
