9fb6b35ed36e2f8587f0b3c565560978efab85b6
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-12 14:02:57 -0300

    Capitulo 5 Parte 7

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,6 +1,8 @@
 import { DomainEvents } from "@/core/events/domain-events";
+import { envSchema } from "@/infra/env/env";
 import { PrismaClient } from "@prisma/client";
 import { config } from "dotenv";
+import { Redis } from "ioredis";
 import { execSync } from "node:child_process";
 import { randomUUID } from "node:crypto";
 
@@ -8,13 +10,21 @@ config({ path: ".env", override: true });
 config({ path: ".env.test", override: true });
 const prisma = new PrismaClient();
 
+const env = envSchema.parse(process.env);
+
+const redis = new Redis({
+    host: env.REDIS_HOST,
+    port: env.REDIS_PORT,
+    db: env.REDIS_DB,
+});
+
 const schemaId = randomUUID();
 
 function generateUniqueDatabaseURL(schemaId: string) {
-    if (!process.env.DATABASE_URL) {
+    if (!env.DATABASE_URL) {
         throw new Error("DATABASE_URL not found in environment variables");
     }
-    const url = new URL(process.env.DATABASE_URL);
+    const url = new URL(env.DATABASE_URL);
     url.searchParams.set("schema", schemaId);
     return url.toString();
 }
@@ -23,6 +33,7 @@ beforeAll(async () => {
     const databaseUrl = generateUniqueDatabaseURL(schemaId);
     process.env.DATABASE_URL = databaseUrl;
     DomainEvents.shouldRun = false;
+    await redis.flushdb();
     execSync("pnpm prisma migrate deploy");
 });
 
